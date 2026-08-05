cb06f1c7e25543abaf6d02964e945ad498ac3d7b
Rohit Bansal <coderrohit02@gmail.com>
2026-08-05 12:36:26 +0530

    Time: 4 ms (34.38%), Space: 9.9 MB (26.26%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        unordered_map<char, int> mp ;
+
+        if(s.size() != t.size()){
+            return false ;
+        }
+        
+        for(char c : s){
+            mp[c]++ ;
+        }
+
+        for(char c : t){
+            if(mp[c] == 0){
+                return false ;
+            }
+            else{
+                mp[c]-- ;
+            }
+        }
+
+        return true ;
+
+    }
+};
